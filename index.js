dbf1f5be125384d2e8edbc2c47415bf831747abc
Adong-o <adongo969@gmail.com>
2023-03-29 09:46:49 +0300

    The Kitchen Site Desktop

---
+++ index.js
@@ -0,0 +1,11 @@
+function openNav(){
+    document.getElementById("mobile-menu").style.width = "300px";
+    document.getElementById("content-area").style.marginLeft = "300px";
+  }
+  
+  function closeNav(){
+    document.getElementById("mobile-menu").style.width = "0";
+    document.getElementById("content-area").style.marginLeft = "0";
+  }
+  
+  
